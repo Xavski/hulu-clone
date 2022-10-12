bd8df66a1b82a4bf985b28a4c9b3e85f5b8bc8a6
Xavski <xavioursaveski1@gmail.com>
2022-10-12 17:49:58 +1100

    priority

--- components/Thumbnail.jsx
+++ components/Thumbnail.jsx
@@ -8,6 +8,7 @@ const Thumbnail = forwardRef(({ result }, ref) => {
     <div ref ={ref} className="group cursor-pointer p-2 transition duration-200 ease-in transform sm:hover:scale-105 hover:z-50">
       <Image
         layout="responsive"
+        priority
         src={
           `${BASE_URL}${result.backdrop_path || result.poster_path}` ||
           `${BASE_URL}${result.poster_path}`
